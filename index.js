7174976b3187d7dcf18de7119e8897e77eeea403
bzayn02 <nagarkotibezay@gmail.com>
2023-06-04 11:36:27 +1000

    reading file system

--- index.js
+++ index.js
@@ -8,22 +8,18 @@ import fs from 'fs';
 const _dirname = path.resolve();
 
 const fn = _dirname + '/userList.csv';
-console.log(_dirname);
 
 // middleware
-
 app.use(express.urlencoded());
 
 app.get('/registration', (req, res) => {
-  console.log(req.query);
-  //   res.send(`<h1>You are in registration page.</h1>`);
-
   res.sendFile(_dirname + '/src/regForm.html');
 });
+
 app.post('/registration', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const { email, password } = req.body;
-  const str = `${email},${password} \n`;
+  const str = `${email},${password}\n`;
   fs.appendFile(fn, str, (error) => {
     error ? console.log(error.message) : console.log('added to file.');
   });
@@ -34,8 +30,28 @@ app.post('/registration', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  console.log('received req to login');
-  res.send('<h1>You are in login page</h1>');
+  res.sendFile(_dirname + '/src/login.html');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const str = `${email},${password}`;
+  console.log(str);
+
+  // readfile
+
+  fs.readFile(fn, (error, data) => {
+    error && console.log(error.message);
+    // console.log(data.toString());
+    const userStr = data.toString();
+    const userArg = userStr.split('\n');
+
+    if (userArg.includes(str)) {
+      res.send('<H1>Login successful.</H1>');
+    } else {
+      res.send('<h1>Invalid login</h1>');
+    }
+  });
 });
 
 // root router, home page
@@ -44,7 +60,9 @@ app.get('/', (req, res) => {
   console.log('received req to home router');
   res.send(`<h1>You are in home page.</h1>
   <a href="/registration">
-  <button>Register</button></a>`);
+  <button>Register</button></a>
+  <a href="/login">
+  <button>Login</button></a>`);
 });
 
 // make our server available on http request
